b7a6fcfd20fc9d145aa12e508c7edac593c9a6aa
MY PC <dpmtpeiris@gmail.com>
2020-03-27 20:58:14 +0530

    lab sheet 4

---
+++ app/src/main/java/com/example/labsheet04/Main2Activity.java
@@ -0,0 +1,29 @@
+package com.example.labsheet04;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main2Activity extends AppCompatActivity {
+
+    private static final String CHANNEL_ID = "100CH";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button cancelButton = (Button) findViewById(R.id.button2);
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cancelIntent = new Intent(Main2Activity.this, MainActivity.class);
+                startActivity(cancelIntent);
+            }
+        });
+    }
+}
